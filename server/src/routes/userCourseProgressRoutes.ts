40bc2431e9292e3d17dd3248d4964a95e821b65e
shashwatdwi176 <dwivedishashwat176@gmail.com>
2025-03-02 20:44:33 +0530

    Added course progress

---
+++ server/src/routes/userCourseProgressRoutes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { getUserEnrolledCourses , getUserCourseProgress ,updateUserCourseProgress } from "../controllers/userCourseProgressController";
+
+const router = express.Router();
+
+router.get("/:userId/enrolled-courses", getUserEnrolledCourses);
+router.get("/:userId/courses/:courseId", getUserCourseProgress);
+router.put("/:userId/courses/:courseId", updateUserCourseProgress);
+
+export default router;
